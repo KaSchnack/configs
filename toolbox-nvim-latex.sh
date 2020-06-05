304dfb165c778bf4775b00858b65f64f2bd0014d
KaSchnack <66488380+KaSchnack@users.noreply.github.com>
2020-06-05 23:01:17 +0400

    Update toolbox-nvim-latex.sh

--- toolbox-nvim-latex.sh
+++ toolbox-nvim-latex.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/bash
 
-echo "## Setting personal gnome inputs..."
+echo "## Changing gnome settings..."
 # Swaps Esc button with CapsLock
 gsettings set org.gnome.desktop.input-sources xkb-options "['caps:swapescape']"
 
 # Enables touchpad while writting
 gsettings set org.gnome.desktop.peripherals.touchpad disable-while-typing "false"
+
+echo "## Installing vim-plug..."
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "## Installing personal neovim init.vim..."
+curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/init.vim --create-dirs https://raw.githubusercontent.com/KaSchnack/configs/master/init.vim
 
 echo "## Creating work container..."
 toolbox create -c work 
@@ -14,11 +20,5 @@ run="toolbox run -c work"
 
 echo "## Installing RPM dependencies inside container..."
 $run sudo dnf install -y nodejs neovim ctags python3-neovim zathura zathura-plugins-all texlive-scheme-full rubber  
-
-echo "## Installing vim-plug..."
-curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-echo "## Installing personal neovim init.vim..."
-curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/init.vim --create-dirs https://raw.githubusercontent.com/KaSchnack/configs/master/init.vim
 
 echo "## Done!"
