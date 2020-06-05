8783db9db53110e63c323751874a76d4d5ec1d9e
KaSchnack <66488380+KaSchnack@users.noreply.github.com>
2020-06-05 22:21:01 +0400

    Add files via upload

--- toolbox-nvim-latex.sh
+++ toolbox-nvim-latex.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash -e
+#!/usr/bin/bash
 
 echo "## Setting personal gnome inputs..."
 # Swaps Esc button with CapsLock
@@ -16,5 +16,7 @@ echo "## Installing RPM dependencies inside container..."
 $run sudo dnf install -y nodejs nvim ctags python3-neovim zathura zathura-plugins-all texlive-scheme-full rubber  
 
 echo "## Installing vim-plug..."
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "## Installing personal neovim init.vim..."
+curl -fLo "${XDG_DATA_HOME:-$HOME/.config}"/nvim/init.vim --create-dirs https://raw.githubusercontent.com/KaSchnack/configs/master/init.vim
